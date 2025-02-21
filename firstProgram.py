1cc0b4ddbed4308e5f543e974b9b63d4f2fd9295
Kaushar45 <45kaushar@gmail.com>
2025-02-21 12:17:22 +0530

    first program of python

---
+++ firstProgram.py
@@ -0,0 +1,39 @@
+print("Hello world")
+print("Hello kaushar")
+val= input("my name is :") #result for input() is always str
+print(type(val)) #str
+# variables
+name = "kaushar"
+age = 20
+
+int()
+float()
+
+# type conversion
+a,b=2,4
+sum=a+b
+print(sum)
+
+# write a program to input 2 numbers & print their sum
+num1=int(input("num1="))
+num2=int(input("num2="))
+print(num1+num2)
+
+
+# write a program to input side of square and print its area.
+size=float(input("size:"))
+print("area =",size**2)
+
+
+# write a program input 2 floating point number & print their average.
+num1=float(input("num1="))
+num2=float(input("num2="))
+avg=(num1+num2)/2
+print(avg)
+
+# write a program to input 2 numbers,a and b.
+# print True if a is greater than or equal to b if not print False.
+
+a=int(input("first num :"))
+b=int(input("second num :"))
+print(a>=b)
